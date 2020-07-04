01b0b267012c926904373327361b5a934ac065db
IdrisShittu <shittuidris45@yahoo.com>
2020-07-04 22:31:09 +0100

    Create minMaxSum.java

---
+++ hackerRank/minMaxSum.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the miniMaxSum function below.
+    static void miniMaxSum(int[] arr) {
+        Arrays.sort(arr);
+        Long min=0L;
+        Long max=0L;
+        int n = arr.length;
+        for(int i=0; i<n; i++){
+            
+            if((i!=n-1))min=min+arr[i];
+            if((i!=0))max=max+arr[i];
+        }
+
+        System.out.println(min+" "+max);
+
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < 5; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        miniMaxSum(arr);
+
+        scanner.close();
+    }
+}
