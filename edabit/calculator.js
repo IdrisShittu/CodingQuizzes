1996c1ad30d549be2529361f041a5e94d9127843
IdrisShittu <shittuidris45@yahoo.com>
2020-08-14 15:10:17 +0100

    performs arithmetic operation

---
+++ edabit/calculator.js
@@ -0,0 +1,14 @@
+function calculator(num1, operator, num2) {
+	if(operator == '+'){
+		return num1 + num2;
+	}else if(operator == '-'){
+		return num1 - num2;
+	}else if(operator == '*'){
+		return num1 * num2;
+	}else if(operator == '/'){
+		if(num2 != 0){
+			return num1 / num2;
+		}
+		return "Can't divide by 0!";
+	}
+}
