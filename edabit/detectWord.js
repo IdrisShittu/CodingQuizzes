7f89735078b57cfbc01009caaca0289d7e95c1d4
IdrisShittu <shittuidris45@yahoo.com>
2020-08-16 13:44:35 +0100

    countTrue
    a function which returns the number of true values there are in an array.

--- edabit/detectWord.js
+++ edabit/detectWord.js
@@ -1,8 +1,8 @@
 function detectWord(str) {
-	var str1 = str;
+	var st1 = str;
 	var x = "";
-	for (i = 0; i < str1.length; i++) {
-		var y = str1.substring(i, (i+1));
+	for (i = 0; i < st1.length; i++) {
+		var y = st1.substring(i, (i+1));
 		if (y == y.toLowerCase()) {
 			console.log(y);
 			x = x + y;
