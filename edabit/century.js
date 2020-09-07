832f9f5feecb4b7b520935131c09265fe9921c34
Idris Shittu <shittuidris45@gmail.com>
2020-09-07 15:02:21 +0100

    centuryfromyear

---
+++ edabit/century.js
@@ -0,0 +1,10 @@
+function century(year) {
+	if (year > 2000) {
+		return "21st century";
+	} else if (year % 100 === 0) {
+		return year / 100 + "th century";
+	} else {
+		var remain = year % 100;
+		return ((year + 100) - remain) / 100 + "th century";
+	}
+}
