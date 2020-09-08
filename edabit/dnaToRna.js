0d9607469a6048480049c430b56be1c7281ccb37
Idris Shittu <shittuidris45@gmail.com>
2020-09-08 15:03:26 +0100

    dnaTorna

---
+++ edabit/dnaToRna.js
@@ -0,0 +1,15 @@
+function dnaToRna(dna) {
+	let result = "";
+	for(let i = 0; i < dna.length; i++) {
+		if(dna[i] === "A") {
+			result += "U"
+		} else if(dna[i] === "T") {
+			result += "A"
+		} else if(dna[i] === "G") {
+			result += "C"
+		} else if(dna[i] === "C") {
+			result += "G"
+		}
+	}
+	return result
+}
