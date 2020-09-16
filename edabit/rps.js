409e1f9c13e15e42909d44e699b6e0a4d401ec00
Idris Shittu <shittuidris45@gmail.com>
2020-09-16 10:40:34 +0100

    rps game winner using if

---
+++ edabit/rps.js
@@ -0,0 +1,21 @@
+function rps(p1, p2) {
+    if (p1 === p2) {
+        return "It's a draw";
+    }
+    if (p1 === 'Rock') {
+        if (p2 === 'Paper') {
+            return 'The winner is p2';
+        }
+    }
+    if (p1 === 'Paper') {
+        if (p2 === 'Scissors') {
+            return 'The winner is p2';
+        }
+    }
+    if (p1 === 'Scissors') {
+        if (p2 === 'Rock') {
+            return 'The winner is p2';
+        }
+    }
+    return 'The winner is p1';
+}
