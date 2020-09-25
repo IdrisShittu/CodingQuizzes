2a5585bb975e861db1aef073d026d243d2a790d2
Idris Shittu <shittuidris45@gmail.com>
2020-09-25 20:08:10 +0100

    get difference in days

---
+++ edabit/getDays.js
@@ -0,0 +1,7 @@
+function getDays(date1, date2) {
+    const ONE_DAY = 1000 * 60 * 60 * 24;
+    // Calculate the difference in milliseconds
+    const differenceMs = Math.abs(date1 - date2);
+    // Convert back to days and return
+    return Math.round(differenceMs / ONE_DAY);
+}
