4c52ee26fd9656813ef24d8d357d7ccbad359dc9
Idris Shittu <shittuidris45@gmail.com>
2020-10-06 12:24:00 +0100

    Create leader.js

---
+++ edabit/leader.js
@@ -0,0 +1,14 @@
+function leader(arr) {
+  const result = [];
+  arr.reverse();
+  let min = arr[0];
+  result.push(arr[0]);
+  for(let i = 1; i < arr.length; i++) {
+    if (arr[i] > min) {
+      min = arr[i];
+      result.push(min);
+    }
+  }
+  return result.reverse();
+}
+//
