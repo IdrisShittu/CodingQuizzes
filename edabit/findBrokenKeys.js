3da0c584bacb5bd4bfce246bab58deca9a5a2422
Idris Shittu <shittuidris45@gmail.com>
2020-12-01 08:03:49 +0100

    Update findBrokenKeys.js

--- edabit/findBrokenKeys.js
+++ edabit/findBrokenKeys.js
@@ -1,9 +1,9 @@
 function findBrokenKeys(str1, str2) {
-	let diffs = [];
-	for (let i = 0; i < str1.length; i++){
-		if (str1[i] != str2[i]){
-			diffs.push(str1[i]);
+	let brokenSet=new Set();
+	for(let i=0;i<str1.length;i++){
+		if(str1[i]!==str2[i]){
+			brokenSet.add(str1[i]);
 		}
 	}
-	return [...new Set(diffs)];
+	return [...brokenSet];
 }
